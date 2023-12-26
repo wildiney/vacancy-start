08269e1c9ee96b6ac03bdaf227dd29191bedf488
WildineyDi Masi <wildiney@gmail.com>
2023-12-26 16:22:47 -0300

    update date

--- app/constants/constants.ts
+++ app/constants/constants.ts
@@ -1,5 +1,5 @@
 const bgImage = 'https://images.unsplash.com/photo-1507295386538-ddd5e86cd597?auto=format&fit=crop&q=80&w=1932&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-const vacationTime = 'jan 03, 2024 18:00:00'
+const vacationTime = 'jan 02, 2024 18:00:00'
 
 export {
     bgImage,
